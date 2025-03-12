f1902b00ec51dd50097e217059d339fdae34db66
Iuri Ferreira <iurifdev@gmail.com>
2025-03-12 17:01:44 -0300

    Initial Commit

---
+++ ToDoTaskApp/ToDoTaskAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTaskAppApp.swift
+//  ToDoTaskApp
+//
+//  Created by User on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoTaskAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
